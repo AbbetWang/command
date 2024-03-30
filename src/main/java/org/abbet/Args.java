5df3e438f017d6c6d5b2945c4b1e36388d328c2b
AbbetWang <AbbetWang@gmail.com>
2024-03-30 12:00:11 +0800

    refacotr:

--- src/main/java/org/abbet/Args.java
+++ src/main/java/org/abbet/Args.java
@@ -6,8 +6,8 @@ public class Args {
     public static <T> T parse(Class<T> optionsClass, String ...args) {
         try {
             return (T) optionsClass.getDeclaredConstructors()[0].newInstance(true);
-        }catch (Exception err){
-            throw new RuntimeException(err);
+        }catch (Exception e){
+            throw new RuntimeException(e);
         }
     }
 }
