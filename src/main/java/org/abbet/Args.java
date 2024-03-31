426833e165e6e83686c7c47f75640f26103deb5a
AbbetWang <AbbetWang@gmail.com>
2024-03-31 11:02:07 +0800

    add: support multi

--- src/main/java/org/abbet/Args.java
+++ src/main/java/org/abbet/Args.java
@@ -1,7 +1,6 @@
 package org.abbet;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
@@ -9,27 +8,29 @@ import java.util.List;
 public class Args {
     public static <T> T parse(Class<T> optionsClass, String... args) {
         try {
-
-            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-            Parameter[] parameters = constructor.getParameters();
-            Parameter parameter = parameters[0];
-            Object value = null;
-            Option option = parameter.getAnnotation(Option.class);
             List<String> arguments = Arrays.asList(args);
-            if(parameter.getType() == boolean.class){
-                value = arguments.contains("-" + option.value());
-            }
-            if(parameter.getType() == int.class){
-                int index = arguments.indexOf("-"+option.value());
-                value = Integer.parseInt(arguments.get(index+1));
-            }
-            if(parameter.getType() == String.class){
-                int index = arguments.indexOf("-"+option.value());
-                value = String.valueOf(arguments.get(index+1));
-            }
-            return (T) constructor.newInstance(value);
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(it, arguments)).toArray();
+            return (T) constructor.newInstance(values);
         }catch (Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    private static Object parseOption(Parameter parameter, List<String> arguments) {
+        Object value = null;
+        Option option = parameter.getAnnotation(Option.class);
+        if(parameter.getType() == boolean.class){
+            value = arguments.contains("-" + option.value());
+        }
+        if(parameter.getType() == int.class){
+            int index = arguments.indexOf("-"+option.value());
+            value = Integer.parseInt(arguments.get(index+1));
+        }
+        if(parameter.getType() == String.class){
+            int index = arguments.indexOf("-"+option.value());
+            value = String.valueOf(arguments.get(index+1));
+        }
+        return value;
     }
 }
--- src/test/java/org/abbet/ArgTest.java
+++ src/test/java/org/abbet/ArgTest.java
@@ -35,6 +35,14 @@ public class ArgTest {
     }
     static record StringOption(@Option("d")String directory){}
     //TODO: multi options: -l -p 8080 -d /usr/logs
+    @Test
+    public void should_parse_multi_option(){
+        MultiOptions options = Args.parse(MultiOptions.class,"-l","-p","8080","-d","/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080,options.port());
+        assertEquals("/usr/logs",options.directory());
+    }
+    static record MultiOptions(@Option("l")boolean logging, @Option("p")int port, @Option("d")String directory){}
     // sad path:
     //TODO: -bool -l t / -l t f
     //TODO: - int -p 8080 9080
@@ -45,20 +53,11 @@ public class ArgTest {
     //TODO: - string: ""
     @Test
     @Disabled
-    public void should_1(){
-       Options options = Args.parse(Options.class,"-l","-p","8080","-d","/usr/logs");
-       assertTrue(options.logging());
-       assertEquals(8080,options.port());
-       assertEquals("/usr/logs",options.directory());
-    }
-    @Test
-    @Disabled
     public void should_2(){
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
         assertArrayEquals(new String[]{"this","is","a","list"},options.group());
         assertArrayEquals(new int[]{1,2,-3,5},options.decimals());
     }
-    static record Options(@Option("l")boolean logging,@Option("p")int port,@Option("d")String directory){}
     static record ListOptions(@Option("g")String[] group,@Option("d")int[] decimals){}
 
 }
