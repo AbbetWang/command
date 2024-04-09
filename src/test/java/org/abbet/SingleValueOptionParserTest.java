6e7f383aa0ce17f06843c9f555f560762a24863e
linux <linux@40.com>
2024-04-09 09:08:55 +0800

    refactor: delete todo comment

--- src/test/java/org/abbet/SingleValueOptionParserTest.java
+++ src/test/java/org/abbet/SingleValueOptionParserTest.java
@@ -8,9 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SingleValueOptionParserTest {
-    // SingleValueOptionParserTest
-    // sad path:
-    //TODO: - int -p 8080 9080
+    
     @Test
     public void should_not_accept_extra_argument_for_integer_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
